c531db897a1153afdcda44e80a39b2fbdaae65ee
luiz Victorino <luiz.apvictorino@gmail.com>
2024-06-22 21:45:53 -0300

    feat: inclusão quantidade na lista de produtos

--- src/components/ListaDeProdutos.js
+++ src/components/ListaDeProdutos.js
@@ -7,6 +7,7 @@ import TaskAPI from './TaskAPI';
 const ListaDeProdutos = () => {
   const [produtos, setProdutos] = useState([]); // Estado para armazenar os produtos do usuário
   const [novoProduto, setNovoProduto] = useState(''); // Estado para o novo produto a ser adicionado
+  const [novaQtde, setNovaQtde] = useState(1); // Estado para a quantidade do novo produto
   const [mostrarFormulario, setMostrarFormulario] = useState(false); // Estado para controlar exibição do formulário
   const userId = firebase.auth().currentUser?.uid; // Obtém o ID do usuário atualmente autenticado
 
@@ -32,12 +33,13 @@ const ListaDeProdutos = () => {
   // Função para adicionar um novo produto
   const adicionarProduto = async () => {
     try {
-      // Cria um novo produto no Firestore com a descrição fornecida pelo usuário
-      const newTaskId = await TaskAPI.createTask({ descricao: novoProduto }, userId);
+      // Cria um novo produto no Firestore com a descrição e quantidade fornecidas pelo usuário
+      const newTaskId = await TaskAPI.createTask({ descricao: novoProduto, quantidade: novaQtde }, userId);
       // Atualiza o estado de produtos adicionando o novo produto
-      const updatedProdutos = [...produtos, { id: newTaskId, descricao: novoProduto }];
+      const updatedProdutos = [...produtos, { id: newTaskId, descricao: novoProduto, quantidade: novaQtde }];
       setProdutos(updatedProdutos);
       setNovoProduto(''); // Limpa o campo de entrada do novo produto
+      setNovaQtde(1); // Reseta a quantidade para o valor padrão
       setMostrarFormulario(false); // Fecha o formulário de adição de produto
     } catch (error) {
       console.error('Erro ao adicionar produto:', error);
@@ -69,6 +71,11 @@ const ListaDeProdutos = () => {
             onChange={(e) => setNovoProduto(e.target.value)}
             placeholder='Digite um novo produto'
           />
+          <input
+            type='number'
+            value={novaQtde}
+            onChange={(e) => setNovaQtde(e.target.value)}
+          />
           <button onClick={adicionarProduto}>Adicionar</button>
         </div>
       )}
@@ -80,7 +87,9 @@ const ListaDeProdutos = () => {
       <ul>
         {produtos.map((produto) => (
           <li key={produto.id} className='produto'>
-            <div>{produto.descricao}</div>
+            <div>
+              {produto.descricao} - Quantidade: {produto.quantidade}
+            </div>
             {/* Botão para remover o produto, que chama a função removerProduto com o ID do produto */}
             <div className='remover-produto' onClick={() => removerProduto(produto.id)}>
               Excluir
